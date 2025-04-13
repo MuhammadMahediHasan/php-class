3b7cb0e51fe234102763de7a641f720794ed709c
MuhammadMahediHasan <mahadih727@gmail.com>
2025-04-13 22:23:25 +0600

    Home page

--- blog-site/admin/post/store.php
+++ blog-site/admin/post/store.php
@@ -5,12 +5,11 @@ include __DIR__ . '/../common/db-connection.php';
 $name =  $_FILES['image']['name'];
 $imagePath = '';
 
-$isFileMoved = move_uploaded_file($_FILES['image']['tmp_name'], "../../storage/posts/$name");
-
 if(file_exists("../../storage/posts/$name")) {
     die("Image already exists!");
 }
 
+$isFileMoved = move_uploaded_file($_FILES['image']['tmp_name'], "../../storage/posts/$name");
 if($isFileMoved) {
     $imagePath = "posts/$name";
 }
@@ -18,10 +17,11 @@ if($isFileMoved) {
 $category_id = $_POST['category_id'];
 $title = $_POST['title'];
 $description = $_POST['description'];
+$createdAt = date('Y-m-d');
 
 $isStored = mysqli_query($db_connection, "
-    INSERT INTO posts (category_id,  title, description, image) 
-    VALUES ('$category_id', '$title', '$description', '$imagePath')"
+    INSERT INTO posts (category_id,  title, description, image, created_at) 
+    VALUES ('$category_id', '$title', '$description', '$imagePath', '$createdAt')"
 );
 
 header("location: ../../admin/?page=create-post");
